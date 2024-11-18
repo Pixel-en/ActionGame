e658ab3b992214ad7d8f7e40c044d6b0fedf54f0
琉聖 大友 <230641@jc-21.jp>
2024-11-18 16:56:52 +0900

    a

--- Server/Server/Engine/Global.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-//安全にメモリを開放するためのマクロ
-#define SAFE_DELETE(p) {if ((p)!=nullptr) { delete (p); (p)=nullptr;}}
-#define SAFE_DELETE_ARRAY(p) {if ((p)!=nullptr) { delete[] (p); (p)=nullptr;}}
-#define SAFE_RELEASE(p) {if ((p)!=nullptr) { p->Release(); (p)=nullptr;}}
